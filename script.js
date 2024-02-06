d38b5ecdc9b851e49bb8087cdfaedb44f196a8d6
Guilherme Pilz Moreira <gui.pilz.2007@gmail.com>
2024-02-05 21:13:29 -0300

    Correção do menu no mobile
    Fontes maiores e remoção da animação pois estava com muitas falhas

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const menu = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav');
+const windows = document.querySelectorAll('a');
+windows.forEach(window => {
+    window.addEventListener('click', ()=> nav.classList.toggle('active'));
+});
 
 menu.addEventListener('click', ()=> nav.classList.toggle('active'));
